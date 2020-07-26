1170ace257ab896d6d93809e7c949fc24215ecd3
Niklas Wigertz <niklas.wigertz@briteback.com>
2020-07-26 20:33:39 +0200

    fix: change network mode to host for ffmpeg-camera

--- ffmpeg-camera/entrypoint.sh
+++ ffmpeg-camera/entrypoint.sh
@@ -2,5 +2,5 @@
 
 : ${RTSP_SERVER_URL?"RTSP_SERVER_URL is required"}
 
-echo 'Starting ffmpeg stream'
+echo "Starting ffmpeg stream to ${RTSP_SERVER_URL}"
 ffmpeg -f v4l2 -input_format yuyv422 -i /dev/video -c:v libx264 -vf format=yuv420p -preset ultrafast -tune zerolatency -crf 21 -f rtsp ${RTSP_SERVER_URL}
